da2f206fb33f4e8190e46a5477c704a6c6db85ff
galipdogan <galipdogan@outlook.com>
2021-06-28 22:22:37 +0300

    Ders 14

--- src/services/productService.js
+++ src/services/productService.js
@@ -7,8 +7,4 @@ export default class ProductService{
     getByProductName(productName){
       return axios.get("http://localhost:8080/api/products/getByProductName?productName="+ productName)
   }
-
-  productAdd(product){
-    return axios.post("http://localhost:8080/api/products/add")
-  }
 }
